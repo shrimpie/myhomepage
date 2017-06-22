20a071e769ac88d26b6740f82dcdc461c5ac7baa
杨振宇 <1210517@tongji.edu.cn>
2017-06-22 11:23:09 -0400

    changed database config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
-
-module.exports = {
-  database: 'mongodb://' +
-            process.env.MONGODB_USER + ':' +
-            process.env.MONGODB_PASSWORD +
-            '@ds027335.mlab.com:27335/nodesample',
-  secret: 'somepassword'
-}
+// require('dotenv').config();
 
 // module.exports = {
-//   database: 'mongodb://localhost:27017/myhome',
-//   secret: 'somesecret'
+//   database: 'mongodb://' +
+//             process.env.MONGODB_USER + ':' +
+//             process.env.MONGODB_PASSWORD +
+//             '@ds027335.mlab.com:27335/nodesample',
+//   secret: 'somepassword'
 // }
+
+
+module.exports = {
+  database: 'mongodb://nodeuser:tnTYlESuHhL3K6sB@ds027335.mlab.com:27335/nodesample',
+  secret: 'somesecret'
+}
